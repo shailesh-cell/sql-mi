29b0915b4ccea897cc3a2b06886c5f8cdfce5467
sc1 <s.c1@mail.com>
2025-03-09 21:46:25 +0530

    inital commit

--- main.tf
+++ main.tf
@@ -4,12 +4,17 @@ module "resource_group" {
 
 module "vnet" {
   source = "./modules/vnet"
+  resource_group_name = var.resource_group_name
 }
 
 module "subnet" {
   source = "./modules/subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.vnet_name
 }
 
 module "sql_mi" {
   source = "./modules/sql_mi"
+
+  subnet_id                    = azurerm_subnet.subnet.id
 }
