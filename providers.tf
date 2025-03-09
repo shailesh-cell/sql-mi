4b1fa47002587dd9b23e8138c45c270bf6ca3cc1
sc1 <s.c1@mail.com>
2025-03-09 23:02:14 +0530

    inital commit

---
+++ providers.tf
@@ -0,0 +1,10 @@
+provider "azurerm" {
+  features {
+    template_deployment {
+      delete_nested_items_during_deletion = true
+    }
+  }
+
+  subscription_id = "a6435a3c-312e-41ed-878b-3118525f919a"
+  tenant_id       = "21150d7d-bb5a-486f-86af-73c82a8240bc"
+}
