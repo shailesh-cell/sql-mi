101fe5a7fd4aa699ced78e697e2eddbd05659865
sc1 <s.c1@mail.com>
2025-03-09 23:19:14 +0530

    inital commit

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,7 +1,7 @@
 # Virtual Network Module (main.tf)
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.product}-${var.environment}-${var.location}-vnet"
-  location            = var.location
+  location            = lookup(local.location_map, var.location, "centralindia")
   resource_group_name = var.resource_group_name
   address_space       = [var.vnet_address_space]
 }
