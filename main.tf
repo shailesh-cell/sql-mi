5eba2d84d583f4e9eb6198765b6e7f015478e092
sc1 <s.c1@mail.com>
2025-03-09 22:03:50 +0530

    inital commit

--- main.tf
+++ main.tf
@@ -4,17 +4,17 @@ module "resource_group" {
 
 module "vnet" {
   source              = "./modules/vnet"
-  resource_group_name = module.resource_group.rg_name
+  resource_group_name = module.resource_group.resource_group_name
 }
 
 module "subnet" {
   source              = "./modules/subnet"
-  resource_group_name = module.resource_group.rg_name
+  resource_group_name = module.resource_group.resource_group_name
   vnet_name           = module.vnet.vnet_name
 }
 
 module "sql_mi" {
   source              = "./modules/sql_mi"
-  resource_group_name = module.resource_group.rg_name
+  resource_group_name = module.resource_group.resource_group_name"
   vnet_name           = module.vnet.vnet_name
 }
