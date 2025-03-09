97d2accd6743618d551f037d2a7e0d1721cb2e03
sc1 <s.c1@mail.com>
2025-03-09 22:53:52 +0530

    inital commit

--- main.tf
+++ main.tf
@@ -4,20 +4,3 @@ module "resource_group" {
   environment = var.environment
   location    = var.location
 }
-
-module "vnet" {
-  source              = "./modules/vnet"
-  resource_group_name = module.resource_group.rg_name
-}
-
-module "subnet" {
-  source              = "./modules/subnet"
-  resource_group_name = module.resource_group.rg_name
-  vnet_name           = module.vnet.vnet_name
-}
-
-module "sql_mi" {
-  source              = "./modules/sql_mi"
-  resource_group_name = module.resource_group.rg_name
-  vnet_name           = module.vnet.vnet_name
-}
