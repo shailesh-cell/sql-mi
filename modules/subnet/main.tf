1e3b03859ce9fed6948713006b71d96f0a93b85f
sc1 <s.c1@mail.com>
2025-03-09 22:56:30 +0530

    inital commit

--- modules/subnet/main.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_subnet" "subnet" {
-  name                 = "${var.product}-${var.environment}-sqlsubnet-${var.location}"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = var.vnet_name
-  address_prefixes     = [var.subnet_address_space]
-}
