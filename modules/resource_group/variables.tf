589b6307421145363aa885f84eb0a3e3ecee411f
sc1 <s.c1@mail.com>
2025-03-09 21:35:09 +0530

    inital commit

---
+++ modules/resource_group/variables.tf
@@ -0,0 +1,39 @@
+variable "location" {
+  type    = string
+  default = "cin"
+  validation {
+    condition     = contains(["cin", "cus"], var.location)
+    error_message = "Allowed values: cin, cus."
+  }
+}
+
+# Map short codes to full region names
+locals {
+  location_map = {
+    cin = "centralindia"
+    cus = "centralus"
+  }
+}
+
+# Use the full region name in resource definitions
+output "full_location" {
+  value = lookup(local.location_map, var.location, "centralindia")
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+  validation {
+    condition     = contains(["dev", "uat", "prod"], var.environment)
+    error_message = "Allowed values: dev, uat, prod."
+  }
+}
+
+variable "product" {
+  type    = string
+  default = "dbuh"
+  validation {
+    condition     = contains(["dbuh", "ubuh", "pbuh"], var.product)
+    error_message = "Allowed values: dbuh, ubuh, pbuh."
+  }
+}
