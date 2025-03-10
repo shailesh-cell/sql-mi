7891844aa036b16e3588a1898b0041a64118ee8d
sc1 <s.c1@mail.com>
2025-03-10 20:06:40 +0530

    inital commit

--- modules/sql_mi/main.tf
+++ modules/sql_mi/main.tf
@@ -1,7 +1,7 @@
 # SQL Managed Instance Module (main.tf)
 resource "azurerm_mssql_managed_instance" "sql_mi" {
   name                         = "${var.product}-${var.environment}-sql-${var.location}"
-  location                     = lookup(local.location_map, var.location, "centralindia")
+  location                     = lookup(local.location_map, var.location)
   resource_group_name          = var.resource_group_name
   administrator_login          = var.admin_username
   administrator_login_password = var.admin_password
@@ -11,4 +11,5 @@ resource "azurerm_mssql_managed_instance" "sql_mi" {
   vcores                       = var.vcores
   license_type                 = var.license_type
   storage_account_type         = lookup(var.storage_account_type_map, var.environment)
+  timezone_id                  = var.timezone_id
 }
