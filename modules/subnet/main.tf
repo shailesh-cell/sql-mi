0c74f39d6075935ef15a3ff321db96babe85b0bf
sc1 <s.c1@mail.com>
2025-03-12 11:56:08 +0530

    long term retention

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -3,4 +3,16 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.vnet_name
   address_prefixes     = [var.subnet_address_space]
+  delegation {
+    name = "mssql-mi-delegation"
+
+    service_delegation {
+      name = "Microsoft.Sql/managedInstances"
+
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+        "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"
+      ]
+    }
+  }
 }
--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -4,4 +4,15 @@ resource "azurerm_virtual_network" "vnet" {
   location            = lookup(local.location_map, var.location, "centralindia")
   resource_group_name = var.resource_group_name
   address_space       = [var.vnet_address_space]
+  delegation {
+    name = "mssql-mi-delegation"
+
+    service_delegation {
+      name = "Microsoft.Sql/managedInstances"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+        "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"
+      ]
+    }
+  }
 }
