d9f35280338614cac25f87e5cf3e320c9bff6220
sc1 <s.c1@mail.com>
2025-03-13 19:57:58 +0530

    application gateway

--- modules/subnet/outputs.tf
+++ modules/subnet/outputs.tf
@@ -7,9 +7,9 @@ output "subnet_id" {
 }
 
 output "subnet1_name" {
-  value = azurerm_subnet.subnet.name
+  value = azurerm_subnet.subnet1.name
 }
 
 output "subnet1_id" {
-  value = azurerm_subnet.subnet.id
+  value = azurerm_subnet.subnet1.id
 }
