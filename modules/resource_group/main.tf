0c5d5d61bd9b24487937105c000bfdd07e6d7976
sc1 <s.c1@mail.com>
2025-03-14 00:51:53 +0530

    application gateway

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -5,6 +5,6 @@ module "global_variables" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "${module.global-variables[var.product]}-${module.global-variables[var.environment]}-${module.global-variables[var.location]}-rg"
+  name     = "${base-infrastructure.global-variables[var.product]}-${base-infrastructure.global-variables[var.environment]}-${base-infrastructure.global-variables[var.location]}-rg"
   location = lookup(local.location_map, var.location, "centralindia")
 }
