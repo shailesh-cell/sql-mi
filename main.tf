056c857341dbecf8868044c3ad2321baf1cdcab3
sc1 <s.c1@mail.com>
2025-03-13 18:11:53 +0530

    application gateway

--- main.tf
+++ main.tf
@@ -31,4 +31,5 @@ module "applicationgateway" {
   source      = "./modules/application_gateway"
   vnet_name   = module.vnet.vnet_name
   subnet_name = module.subnet.subnet_name
+  resource_group_name  = module.resource_group.rg_name
 }
