5ea52c2d85e8508108e71a24fd470b38f3ed1607
sc1 <s.c1@mail.com>
2025-03-13 18:34:32 +0530

    application gateway

--- main.tf
+++ main.tf
@@ -30,6 +30,6 @@ module "sqldatabase" {
 module "applicationgateway" {
   source      = "./modules/application_gateway"
   vnet_name   = module.vnet.vnet_name
-  subnet_name = module.subnet.subnet_name
+  subnet_id = module.subnet.subnet_id
   resource_group_name  = module.resource_group.rg_name
 }
