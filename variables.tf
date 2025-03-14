daa47e428d1cd5906fb29d2563a44dcf6d860dbb
sc1 <s.c1@mail.com>
2025-03-14 16:21:37 +0530

    rg

--- variables.tf
+++ variables.tf
@@ -3,9 +3,32 @@ variable "resource_group_name" {
   type        = string
 }
 
-variable "location" {
-  description = "The location of the resource group"
+variable "resource_group_name" {
+  description = "The name of the resource group"
   type        = string
+}
+
+variable "location" {
+  type    = string
+  default = "wus2"
+  validation {
+    condition     = contains(["cin", "cus", "wus2"], var.location)
+    error_message = "Allowed values: cin, cus, wus2."
+  }
+}
+
+# Map short codes to full region names
+locals {
+  location_map = {
+    cin = "centralindia"
+    cus = "centralus"
+    wus2 = "West US 2" 
+  }
+}
+
+# Use the full region name in resource definitions
+output "full_location" {
+  value = lookup(local.location_map, var.location, "centralindia")
 }
 
 variable "tags" {
