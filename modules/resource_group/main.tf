92a5d7152198ceaf92d8f7d38da6d115e712d1b8
sc1 <s.c1@mail.com>
2025-03-14 15:39:38 +0530

    application gateway

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,10 +1,7 @@
 # Resource Group Module (main.tf)
 
-module "global_variables" {
-  source = "../base-infrastructure/global-variables"
-}
-
 resource "azurerm_resource_group" "rg" {
-  name     = "${module.base-infrastructure.global-variables[var.product]}-${module.base-infrastructure.global-variables[var.environment]}-${module.base-infrastructure.global-variables[var.location]}-rg"
+  name     = "var.product-var.environment-var.location-rg"
   location = lookup(local.location_map, var.location, "centralindia")
+  tags     = var.tags
 }
---
+++ modules/resource_group/variables.tf
@@ -0,0 +1,16 @@
+# resource_group/variables.tf
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The location of the resource group"
+  type        = string
+}
+
+variable "tags" {
+  description = "A map of tags to add to the resource group"
+  type        = map(string)
+}
