b56ff05e40edde43f2636bef2147e91da8657ae7
sc1 <s.c1@mail.com>
2025-03-15 12:49:20 +0530

    rg

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,12 +1,7 @@
 # Resource Group Module (main.tf)
 
-module "global-variables" {
-  source = "./modules/global-variables"
-}
-
-
 resource "azurerm_resource_group" "rg" {
-  name     = "${module.global-variables[var.product]}-${module.global-variables[var.environment]}-${module.global-variables[var.location]}-rg"
-  location = lookup(local.location_map, var.location, "centralindia")
-  tags     = "${module.global-variables[var.tags]}"
+  name     = "var.product-var.environment-var.location-rg"
+  location = lookup(local.location_map, var.location)
+  tags     = "var.tags"
 }
