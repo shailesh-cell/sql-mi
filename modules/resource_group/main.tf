130245b23cb8a6d75079c0349ae002d4d47608f4
sc1 <s.c1@mail.com>
2025-03-15 12:53:35 +0530

    rg

--- modules/resource_group/main.tf
+++ modules/resource_group/main.tf
@@ -1,7 +1,7 @@
 # Resource Group Module (main.tf)
 
 resource "azurerm_resource_group" "rg" {
-  name     = "var.product-var.environment-var.location-rg"
+  name     = "${var.product}-${var.environment}-${var.location}-rg"
   location = lookup(local.location_map, var.location)
-  tags     = "var.tags"
+  tags     = var.tags
 }
