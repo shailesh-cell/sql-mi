c29197f6c5879e57ac362407f3e860a92631e520
sc1 <s.c1@mail.com>
2025-03-15 13:13:45 +0530

    asaj

--- modules/stream-analytic-job/main.tf
+++ modules/stream-analytic-job/main.tf
@@ -2,7 +2,7 @@
 
 resource "azurerm_stream_analytics_job" "asaj" {
   name                                     = "${var.product}-${var.environment}-${var.location}-rg"
-  resource_group_name                      = var.resource_group_name.rg_name
+  resource_group_name                      = var.resource_group_name
   location                                 = lookup(local.location_map, var.location, "centralindia")
   compatibility_level                      = "1.2"
   data_locale                              = "en-US"
